5c92ba210a31e7a4bd302a793669af32c8631077
Sudhakar <skadi@motoinsight.com>
2025-06-22 18:50:51 +0530

    Initial commit

---
+++ src/test/java/newOne/FirstTestDemoTest.java
@@ -0,0 +1,92 @@
+package newOne;
+
+
+import java.io.File;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.UsernameAndPassword;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+public class FirstTestDemoTest {
+	
+		ThreadLocal<WebDriver> tdriver = new ThreadLocal<WebDriver>();
+		@Test @Parameters({"browser","URL","FFURL"})
+		public void openBrowserPage(String browser,String URL,String FFURL) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		WebDriver driver = null;
+		//String browser = System.getProperty("browser");
+		System.out.println("BrowerName   " + browser);
+		System.out.println("URL Name    " + URL);
+		System.out.println("URL Name for FireFox   " + FFURL);
+		//nordVPNConnect connect = new nordVPNConnect();
+		ChromeOptions options = new ChromeOptions();
+		//FirefoxOptions FFOptions = new FirefoxOptions();
+		
+		   
+		   //FirefoxProfile profile = new FirefoxProfile();
+		   //profile.setPreference("network.automatic-ntlm-auth.trusted-uris", "shopvolvo.tier3-ps.autosync.cloud");
+		   //profile.setPreference("network.http.phishy-userpass-length", 255);
+		   //FFOptions.setProfile(profile);
+		   //options.addArguments("--incognito");
+		   if ("chrome".equalsIgnoreCase(browser)) {
+		    driver = new ChromeDriver(options);
+		    HasAuthentication authentication = (HasAuthentication) driver; 
+			authentication.register(()-> new UsernameAndPassword("gr33n", "y3ll0w"));
+		} else {
+			 
+		    // Handle other browsers
+				//FFOptions.setProfile(profile);
+				//profile.addExtension(new File("C:\\Users\\Sudhakar\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\xzxcjvsz.selenium_profile\\extensions\\autoauth@efinke.com.xpi"));
+				//FFOptions.setProfile(profile);
+				//FFOptions.setProfile(profile);
+				driver = new FirefoxDriver();
+
+		    }
+		   
+		   tdriver.set(driver);
+			   driver = tdriver.get();
+			   if ("chrome".equalsIgnoreCase(browser)) {
+				   driver.get(URL);
+			   } else {
+				   driver.get(FFURL);
+			   }
+			   
+			   driver.manage().window().maximize();
+			  		
+			// Set up WebDriverWait with timeout of 10 seconds
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(90));
+		
+		// Wait until the element is visible
+		wait.until(
+		    
+		    ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[normalize-space()='Search']"))
+		);
+		System.out.println("Browsers Titile is   " + driver.getTitle());//.contentEquals("Google");
+		int number_pages = driver.findElements(By.cssSelector("ul.NPagination-items > li")).size();
+		
+		System.out.println("Number of Pages are  : " + number_pages);
+		
+		for (int p=2;p<=number_pages;p++) {
+			driver.findElement(By.cssSelector("ul.NPagination-items >li:nth-child("+p+")")).click();
+		}
+		    
+		//driver.close();
+		tdriver.get().quit();
+		tdriver.remove(); // Important to remove the driver from ThreadLocal
+	}
+	
+
+}	
