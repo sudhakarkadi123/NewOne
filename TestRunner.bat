ec8559fbc2d9ca99352ee81f50491cac6c4579e1
sudhakarkadi123 <Sudhakar.sub@gmai.com>
2025-06-22 22:11:47 +0530

    Changes to POM.XML

--- TestRunner.bat
+++ TestRunner.bat
@@ -1,3 +1,3 @@
 nordvpn connect
-mvn test -DURL="https://volvocentral.tier3-ps.autosync.cloud/" -DFFURL="https://gr33n:y3ll0w@volvocentral.tier3-ps.autosync.cloud"
+mvn test -DURL="https://volvocentral.tier3-ps.autosync.cloud/" -DFFURL="https://gr33n:y3ll0w@volvocentral.tier3-ps.autosync.cloud" -P headed
 nordvpn disconnect
