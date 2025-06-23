5c98a4417b29c15c2271d3fbd3f02632059568b5
sudhakarkadi123 <Sudhakar.sub@gmai.com>
2025-06-23 10:33:37 +0530

    Change TestRunner.bat

--- TestRunner.bat
+++ TestRunner.bat
@@ -1,1 +1,3 @@
-mvn test -DURL="https://volvocentral.tier3-ps.autosync.cloud/" -DFFURL="https://gr33n:y3ll0w@volvocentral.tier3-ps.autosync.clou
+nordvpn connect
+mvn test -DURL="https://volvocentral.tier3-ps.autosync.cloud/" -DFFURL="https://gr33n:y3ll0w@volvocentral.tier3-ps.autosync.cloud"
+nordvpn disconnect
